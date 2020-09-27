24d281e6a7a5464de4e2b5061899ed3df5098c2d
bengbengku <anggiatbenget70@gmail.com>
2020-09-28 05:59:48 +0700

    Struktur-retrieve-twitter-data-API

--- app.js
+++ app.js
@@ -1,12 +1,33 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const axios = require("axios");
 
-app.get('/tweets', (req, res) => {
-    console.log(req.query);
-  res.send('Hello World!')
-})
+app.get("/tweets", (req, res) => {
+  const query = req.query.q;
+  const count = req.query.count;
+
+  const url = "https://api.twitter.com/1.1/search/tweets.json";
+  axios
+    .get(url, {
+      params: {
+        q: query,
+        count: count,
+      },
+      headers: {
+        Authorization:
+          "Bearer AAAAAAAAAAAAAAAAAAAAAMYgIAEAAAAAF7Jwby68Lnz5rfRAREqJyFwwSFI%3DmUZzirA3tkhfrtW3TyXzYuUmc1RuzNTJr2r831Syp5YvW78qzU",
+      },
+    })
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+        res.status(400).send(error);
+    });
+
+});
 
 app.listen(port, () => {
-  console.log(`Twitter app listening at http://localhost:${port}`)
-})
+  console.log(`Twitter app listening at http://localhost:${port}`);
+});
