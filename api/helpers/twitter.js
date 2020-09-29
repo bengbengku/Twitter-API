6c541ed090d97e3448e046829535df3084bb6fb9
bengbengku <anggiatbenget70@gmail.com>
2020-09-30 00:38:24 +0700

    tambahan-class-twitter

--- api/helpers/twitter.js
+++ api/helpers/twitter.js
@@ -1,9 +1,18 @@
-
-
+const axios = require('axios');
+const URL = "https://api.twitter.com/1.1/search/tweets.json";
 class Twitter {
 
     get(query, count) {
-
+        return axios.get(URL, {
+            params: {
+              q: query,
+              count: count,
+            },
+            headers: {
+              Authorization:
+                "Bearer AAAAAAAAAAAAAAAAAAAAAMYgIAEAAAAAF7Jwby68Lnz5rfRAREqJyFwwSFI%3DmUZzirA3tkhfrtW3TyXzYuUmc1RuzNTJr2r831Syp5YvW78qzU",
+            },
+          })
     }
 
 
