00f4be7d43bed2b1d3fc39a727b6e32f4358489c
bengbengku <anggiatbenget70@gmail.com>
2020-09-30 00:37:57 +0700

    import-class-twitter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const axios = require("axios");
+const Twitter = require('./api/helpers/twitter');
 
 app.get("/tweets", (req, res) => {
   const query = req.query.q;
